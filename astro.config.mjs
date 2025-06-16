ee53c350eaf84dd845ce1ae96f4bb3b493db4510
Bbllaaddee <mr.kamorzin@yandex.ru>
2025-06-16 20:53:13 +0300

    add preact and greetings

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,10 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
 export default defineConfig({
-  site: "https://stupendous-figolla-82ed11.netlify.app/"
+  site: "https://stupendous-figolla-82ed11.netlify.app/",
+  integrations: [preact()]
 });
